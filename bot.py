48f7c58114fceaa5b91649017079c6c328885ee0
unsaidgried <reguri103@gmail.com>
2026-09-20 09:45:16 +0200

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,22 @@
+import os
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Pong! 🏓")
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f"yo {ctx.author.mention} 👋")
+
+bot.run(os.environ["DISCORD_TOKEN"])
